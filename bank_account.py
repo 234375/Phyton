1876af2b6159ad8d3a4e58cb9a10ea43c4195fa0
Sadya Ibrahim <sadyaibrahim791@gmail.com>
2025-12-11 11:35:59 +0300

    Update bank_account.py

--- bank_account.py
+++ bank_account.py
@@ -1,29 +1,36 @@
+#1. Create a class named BankAccount.
+#2. Add a constructor that sets the account owner's name and starting balance.
+#3. Add a deposit method that increases the balance.
+#4. Add a withdraw method that decreases the balance (only if balance is enough).
+#5. Add a display method to show the owner's name and current balance.
+#6. Create an object of the class (an account).
+#7. Call the methods using the object to deposit, withdraw, and display information.
+
+
+
 class BankAccount:
-    def __init__ (self,owner_name,balance=0):
-        self.owner_name= owner_name
-        self.balance= balance
-        
-   def deposit(self, amount):
+    def __init__(self, owner_name, balance=0):
+        self.owner_name = owner_name
+        self.balance = balance
+
+    def deposit(self, amount):
         if amount <= 0:
-            print(" Amount must be positive!")
+            print("Amount must be positive!")
             return
-       balance += amount
-       print (f' the new balance account is {balance} with deposit of {amount}')
-   def withdraw(self,amount):
-        if amount <= 0:
-            print(" Amount must be positive!")
-            return
+        self.balance += amount
+        print(f'The new balance is {self.balance} with a deposit of {amount}')
+
+    def withdraw(self, amount):
         if amount > self.balance:
-            print(" Insufficient balance.")
+            print("Insufficient balance.")
             return
-        balance -= amount
-        print (f' the new balance account is {balance} with withdraw of {amount}')
+        self.balance -= amount
+        print(f'The new balance is {self.balance} with a withdrawal of {amount}')
+
     def display(self):
-        print (f" owner's name{self.owner_name} , deposit {amount} , withdraw {amount} , balance{ self.balance}")
-    
-    
-    
-account = BankAccount("Meryem" , 2000)
-account.deposit (500)
-account.withdraw(200)
-account.display ()
+        print(f"Owner's name: {self.owner_name}, Balance: {self.balance}")
+
+acc= BankAccount("Rahmet", 3000)
+acc.deposit(1000)
+acc.withdraw(500)
+acc.display()
